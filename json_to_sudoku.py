334cd20f5a0d9389c84a65b507607529b6605aaf
Johan Vischer <sr71lover@yahoo.com>
2017-10-28 07:31:02 -0600

    Refined the process of grid formation with a completely functioning approach.

--- json_to_sudoku.py
+++ json_to_sudoku.py
@@ -5,8 +5,8 @@ class Collider(object):
 
     def __init__(self, x1, y1, x2, y2):
         self.HALF_AVG_SIZE = ((x2 - x1) + (y2 - y1)) / 2
-        self.xMid = (x2 - x1) / 2
-        self.yMid = (y2 - y1) / 2
+        self.xMid = (x2 - x1) / 2 + x1
+        self.yMid = (y2 - y1) / 2 + y1
 
         self.x1 = self.xMid - self.HALF_AVG_SIZE
         self.x2 = self.xMid + self.HALF_AVG_SIZE
@@ -24,6 +24,12 @@ def findSpacesBetweenHoriz(col1, col2):
     yAvg = ((col1.y2 - col1.y1) + (col2.y2 - col2.y1)) / 2 + (col1.y1 + col2.y1) / 2
     x0Mid = ((col1.x2 - col1.x1) / 2)
     xDiff = ((col2.x2 - col2.x1) / 2) - x0Mid
+
+    # Account for cases of the same column
+    print(col2.x1, "<=", col1.x1 + x0Mid)
+    if (col2.x1 <= col1.x1 + x0Mid):
+        return -1
+
     for objs in range(2, 64):
         pastCldr = col1
         for cldrs in range(0, objs):
@@ -40,6 +46,12 @@ def findSpacesBetweenVert(col1, col2):
     xAvg = ((col1.x2 - col1.x1) + (col2.x2 - col2.x1)) / 2 + (col1.x1 + col2.x1) / 2
     y0Mid = ((col1.y2 - col1.y1) / 2)
     yDiff = ((col2.y2 - col2.y1) / 2) - y0Mid
+
+    # Account for cases of the same row
+    print(col2.y1, "<=", col1.y1 + y0Mid)
+    if (col2.y1 <= col1.y1 + y0Mid):
+        return -1
+
     for objs in range(2, 64):
         pastCldr = col1
         for cldrs in range(0, objs):
@@ -66,6 +78,7 @@ if __name__ == '__main__':
                 print(annotations[i]["description"])
                 vertices = annotations[i]["boundingPoly"]["vertices"]
                 col = Collider(vertices[0]["x"], vertices[0]["y"], vertices[1]["x"], vertices[1]["y"])
+                print(vertices[0]["x"], vertices[0]["y"], vertices[1]["x"], vertices[1]["y"], " to ",col.x1, col.y1, col.x2, col.y2)
                 littleSquares.append((annotations[i]["description"], col))
 
             # Test data to make sure it only considers the digits
@@ -83,14 +96,72 @@ if __name__ == '__main__':
             for i in range(len(littleSquares)):
                 print(littleSquares[i])
 
+            # Track smallest and largest entry in x and y
+            if (len(littleSquares) < 2):
+                print("Found 1 valid entry. Exited incorrectly")
+                exit(-1)
+            smallestXEntry = littleSquares[0]
+            largestXEntry = littleSquares[0]
+            smallestYEntry = littleSquares[0]
+            largestYEntry = littleSquares[0]
+            for i in range(len(littleSquares)):
+                if (smallestXEntry[1].x1 > littleSquares[i][1].x1):
+                    smallestXEntry = littleSquares[i]
+                if (largestXEntry[1].x2 < littleSquares[i][1].x2):
+                    largestXEntry = littleSquares[i]
+                if (smallestYEntry[1].y1 > littleSquares[i][1].y1):
+                    smallestYEntry = littleSquares[i]
+                if (largestYEntry[1].y2 < littleSquares[i][1].y2):
+                    largestYEntry = littleSquares[i]
+
+            # Find out size of min grid
+            minGridDimen = 1
+            minGridDimen = max(minGridDimen, findSpacesBetweenHoriz(smallestXEntry[1], largestXEntry[1]) + 2)
+            minGridDimen = max(minGridDimen, findSpacesBetweenVert(smallestYEntry[1], largestYEntry[1]) + 2)
+            print(minGridDimen)
+
+            # Fill in a basic grid
+            grid = []
+            for i in range(minGridDimen):
+                inner = []
+                for j in range(minGridDimen):
+                    inner.append(".")
+                grid.append(inner)
+
+            # Fill in the smallest x and y entries with guaranteed initial x/y and the other components from eachother
+            grid[0][findSpacesBetweenVert(smallestYEntry[1], smallestXEntry[1]) + 1] = smallestXEntry[0]
+            grid[findSpacesBetweenHoriz(smallestXEntry[1], smallestYEntry[1]) + 1][0] = smallestYEntry[0]
+
+            # Find spaces between min x/y to place the rest of the entries in the grid
+            for i in range(len(littleSquares)):
+                if (littleSquares[i] == smallestXEntry or littleSquares[i] == smallestYEntry):
+                    continue
+                else:
+                    x = findSpacesBetweenHoriz(smallestXEntry[1], smallestYEntry[1]) + 1
+                    y = findSpacesBetweenVert(smallestYEntry[1], smallestXEntry[1]) + 1
+                    grid[x][y] = littleSquares[i][0]
+
+            print(minGridDimen)
+            for x in range(minGridDimen):
+                for y in range(minGridDimen):
+                    print(x, y, grid[x][y])
+
+            """
+            row = 0
+            column = 0
             # Cover all cases, with breaks throughout
             for y in range(len(littleSquares) - 1):
                 for x in range(len(littleSquares) - 1):
-                    spaces = findSpacesBetweenHoriz(littleSquares[x][1], littleSquares[x + 1][1])
-                    print("Horiz spaces between", x, y, "is", spaces)
+                    # Stop checking horizontal spaces if clearly on a different row
+                    if ():
+                        row += 1
+                    else:
+                        spaces = findSpacesBetweenHoriz(littleSquares[x][1], littleSquares[x + 1][1])
+                        print("Horiz spaces between", x, y, "is", spaces)
             for y in range(len(littleSquares) - 1):
                 for x in range(len(littleSquares) - 1):
                     spaces = findSpacesBetweenVert(littleSquares[x][1], littleSquares[x + 1][1])
                     print("Vertical spaces between", x, y, "is", spaces)
+            """
     else:
         print("Takes in a single file as input.")
