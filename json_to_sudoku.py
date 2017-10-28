52d836fb3a3c5281d955f7368b05c0fd94140575
Johan Vischer <sr71lover@yahoo.com>
2017-10-28 06:20:11 -0600

    x/y space detection implementation

--- json_to_sudoku.py
+++ json_to_sudoku.py
@@ -5,18 +5,50 @@ class Collider(object):
 
     def __init__(self, x1, y1, x2, y2):
         self.HALF_AVG_SIZE = ((x2 - x1) + (y2 - y1)) / 2
-        xMid = (x2 - x1) / 2
-        yMid = (y2 - y1) / 2
+        self.xMid = (x2 - x1) / 2
+        self.yMid = (y2 - y1) / 2
 
-        self.x1 = xMid - self.HALF_AVG_SIZE
-        self.x2 = xMid + self.HALF_AVG_SIZE
-        self.y1 = yMid - self.HALF_AVG_SIZE
-        self.y2 = yMid + self.HALF_AVG_SIZE
+        self.x1 = self.xMid - self.HALF_AVG_SIZE
+        self.x2 = self.xMid + self.HALF_AVG_SIZE
+        self.y1 = self.yMid - self.HALF_AVG_SIZE
+        self.y2 = self.yMid + self.HALF_AVG_SIZE
 
     def checkCollision(self, col):
         xCol = (self.x1 >= col.x1 and self.x1 <= col.x2) or (self.x2 >= col.x1 and self.x2 <= col.x2)
         yCol = (self.y1 >= col.y1 and self.y1 <= col.y2) or (self.y2 >= col.y1 and self.y2 <= col.y2)
         return xCol and yCol
+
+def findSpacesBetweenHoriz(col1, col2):
+    spaces = 0
+
+    yAvg = ((col1.y2 - col1.y1) + (col2.y2 - col2.y1)) / 2 + (col1.y1 + col2.y1) / 2
+    x0Mid = ((col1.x2 - col1.x1) / 2)
+    xDiff = ((col2.x2 - col2.x1) / 2) - x0Mid
+    for objs in range(2, 64):
+        pastCldr = col1
+        for cldrs in range(0, objs):
+            cldr = Collider(x0Mid + ((cldrs / objs) * xDiff), yAvg, x0Mid + (((cldrs + 1) / objs) * xDiff), yAvg)
+            if (cldr.checkCollision(pastCldr) or cldr.checkCollision(cldr)):
+                return spaces
+            pastCldr = cldr
+        spaces += 1
+    return spaces
+
+def findSpacesBetweenVert(col1, col2):
+    spaces = 0
+
+    xAvg = ((col1.x2 - col1.x1) + (col2.x2 - col2.x1)) / 2 + (col1.x1 + col2.x1) / 2
+    y0Mid = ((col1.y2 - col1.y1) / 2)
+    yDiff = ((col2.y2 - col2.y1) / 2) - y0Mid
+    for objs in range(2, 64):
+        pastCldr = col1
+        for cldrs in range(0, objs):
+            cldr = Collider(xAvg, y0Mid + ((cldrs / objs) * yDiff), xAvg, y0Mid + (((cldrs + 1) / objs) * yDiff))
+            if (cldr.checkCollision(pastCldr) or cldr.checkCollision(cldr)):
+                return spaces
+            pastCldr = cldr
+        spaces += 1
+    return spaces
 
 if __name__ == '__main__':
     if True:
@@ -51,16 +83,14 @@ if __name__ == '__main__':
             for i in range(len(littleSquares)):
                 print(littleSquares[i])
 
-            xMin = 0
-            xMax = 0
-            yMin = 0
-            yMax = 0
-            for i in range(len(littleSquares)):
-                xMin = min(xMin, littleSquares[i][1])
-                xMax = max(xMax, littleSquares[i][1])
-                yMin = min(yMin, littleSquares[i][2])
-                yMax = max(yMax, littleSquares[i][2])
-
-
+            # Cover all cases, with breaks throughout
+            for y in range(len(littleSquares) - 1):
+                for x in range(len(littleSquares) - 1):
+                    spaces = findSpacesBetweenHoriz(littleSquares[x][1], littleSquares[x + 1][1])
+                    print("Horiz spaces between", x, y, "is", spaces)
+            for y in range(len(littleSquares) - 1):
+                for x in range(len(littleSquares) - 1):
+                    spaces = findSpacesBetweenVert(littleSquares[x][1], littleSquares[x + 1][1])
+                    print("Vertical spaces between", x, y, "is", spaces)
     else:
         print("Takes in a single file as input.")
